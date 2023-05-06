a6974b4ba44c925faee25b9023ad92fa9b124cdf
miruna suliman <miruna.suliman@yahoo.com>
2023-05-06 19:27:26 +0300

    Add project files.

---
+++ ChromaticDoodles/Form1.cs
@@ -0,0 +1,32 @@
+﻿namespace ChromaticDoodles
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.BackColor = Color.Cyan;
+            pictureBox2.BackColor = Color.Magenta;
+            pictureBox3.BackColor = Color.Yellow;
+            pictureBox4.BackColor = Color.Black;
+
+        }
+      
+        public event EventHandler ButtonClicked;
+        
+        public void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (ButtonClicked != null)
+             ButtonClicked(sender, e);
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+            this.Parent = null;
+        }
+    }
+}
